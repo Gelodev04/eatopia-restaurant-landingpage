70f862aee03c4949e7c16c25268e8c1fb491e012
gelo <gelodev83@gmail.com>
2025-01-22 17:26:22 +0800

    a

--- src/components/sections/HomePage.tsx
+++ src/components/sections/HomePage.tsx
@@ -8,8 +8,8 @@ export default function HomePage() {
     
       <div id="home" className="min-h-screen ">
         <div className="absolute inset-y-[300px]  inset-[100px]  z-10 flex flex-col items-center justify-center ">
-            <h1 className="text-white text-4xl italic">Welcome to </h1>
-            <span className="text-white text-7xl font-semibold ">Eatopia</span>
+            <h1 className="text-white text-4xl italic text-center">Welcome to </h1>
+            <span className="text-white text-6xl font-semibold lg:text-7xl">Eatopia</span>
             <LookMenu/>
           </div>
         <Carousel />
